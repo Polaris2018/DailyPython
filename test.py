8790be0e8b766ec85e75664907d5e92586f8adfb
Polaris <aileiwang@foxmail.com>
2017-09-23 17:32:40 +0800

    Add test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-09-23 11:14:37
+# @Author  : Your Name (you@example.org)
+# @Link    : http://example.org
+# @Version : $Id$
+
+import os
+import webbrowser
+baidu = 'https://www.baidu.com'
+sina = 'http://www.sina.com.cn'
+
+webbrowser.open(sina)
+def hehe():
+	pass
